a271c71bbc35b107144fe16f06b7a3491d6fe0eb
HanyCZeu <74603656+hanyczeu@users.noreply.github.com>
2022-08-01 08:09:49 +0200

    Update config.lua

--- qb-pernik/config.lua
+++ qb-pernik/config.lua
@@ -6,7 +6,7 @@ Config.Delays = {
 	ProcessPernik = 1000 * 1
 }
 
-Config.Pricesell = 700
+Config.Pricesell = 1250
 
 Config.MinPiecesWed = 1
 
@@ -23,7 +23,7 @@ Config.LicenseEnable = false -- enable processing licenses? The player will be r
 Config.GiveBlack = false -- give black money? if disabled it'll give regular cash.
 
 Config.CircleZones = {
-	PickupPernik = {coords = vector3(3728.7, 3805.07, 5.57), name = 'blip_PickupPernik', color = 25, sprite = 496, radius = 30.0},
-	ProcessPernik = {coords = vector3(2433.94, 4968.69, 42.35), name = 'blip_ProcessPernik', color = 25, sprite = 496, radius = 100.0},
+	PervitinField = {coords = vector3(3728.7, 3805.07, 5.57), name = 'blip_PickupPernik', color = 25, sprite = 496, radius = 30.0},
+	PervitinProcessing = {coords = vector3(2433.94, 4968.69, 42.35), name = 'blip_ProcessPernik', color = 25, sprite = 496, radius = 100.0},
 	DrugDealer = {coords = vector3(2407.75, 3031.64, 48.15), name = 'blip_DrugDealer', color = 6, sprite = 378, radius = 25.0},
 }
